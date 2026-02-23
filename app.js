2b6aad613a397485be3229037defed4784fa91a5
Aman bhai <amanpatel64907@gmail.com>
2026-02-23 07:19:25 +0530

    change at 23 feburary

--- app.js
+++ app.js
@@ -86,8 +86,8 @@ app.get("/allchapter/:semester", (req, res) => {
 const oauth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  link
-     // process.env.REDIRECT_URI,
+   link
+    // process.env.REDIRECT_URI,
   
 );
 const SCOPES = ["https://www.googleapis.com/auth/drive.file"];
